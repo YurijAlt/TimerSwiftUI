4759962381fcb725ad0b9ea02d2efb675849de7f
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-06-13 23:08:28 +0300

    Initial Commit

---
+++ TimerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerSwiftUI
+//
+//  Created by SummeR on 13.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerSwiftUI/TimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIApp.swift
+//  TimerSwiftUI
+//
+//  Created by SummeR on 13.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
